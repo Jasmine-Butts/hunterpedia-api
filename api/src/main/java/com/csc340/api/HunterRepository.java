dd31b5d24e9e2c611e8f97f8d37b3348df8c5cf1
Jasmine Butts <jdbutts@uncg.edu>
2026-03-05 13:38:56 -0500

    setting up repository interface

--- api/src/main/java/com/csc340/api/HunterRepository.java
+++ api/src/main/java/com/csc340/api/HunterRepository.java
@@ -1,5 +1,12 @@
 package com.example.api;
 
-public class HunterRepository {
+import java.util.list;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HunterRepository extends JpaRepository<Hunter, Long> {
     
 }
